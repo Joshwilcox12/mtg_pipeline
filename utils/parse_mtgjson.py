f63997ae4a2a8cbfed1d7b0b95374fbc793ad398
Josh <notlikethis1212@gmail.com>
2025-06-22 22:15:13 -0500

    First initial testings

---
+++ utils/parse_mtgjson.py
@@ -0,0 +1,17 @@
+import json
+import sqlite3
+import pandas as pd
+
+
+
+with open("data/AtomicCards.json", "r", encoding="utf-8") as f:
+
+
+    data = json.load(f)
+    meta = data["meta"]
+    cards = data["data"]
+    card_data = cards.get("Brims")
+    first_five =  list(cards.keys())[:5]
+
+print(card_data)
+
