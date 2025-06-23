7cbe6c1de10a5e68be081847dfa8073d969dc7f8
Josh <notlikethis1212@gmail.com>
2025-06-22 22:59:42 -0500

    Update .gitignore to exclude AtomicCards.json

--- utils/parse_mtgjson.py
+++ utils/parse_mtgjson.py
@@ -10,8 +10,8 @@ with open("data/AtomicCards.json", "r", encoding="utf-8") as f:
     data = json.load(f)
     meta = data["meta"]
     cards = data["data"]
-    card_data = cards.get("Brims")
+    card_data = cards.get('"Brims" Barone, Midway Mobster')
     first_five =  list(cards.keys())[:5]
 
-print(card_data)
+print(card_data[0].keys())
 
