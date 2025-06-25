b4d218f70694bf3a5ecc6a17bd67744253651270
Josh <notlikethis1212@gmail.com>
2025-06-24 22:54:38 -0500

    add each task as function and seperate url

--- utils/parse_mtgjson.py
+++ utils/parse_mtgjson.py
@@ -3,26 +3,26 @@ import sqlite3
 import pandas as pd
 
 
+ #list of keys(columns) I want and think are important for this project
+ #  
+keys = ["setCode", "uuid", "name", "borderColor", "colors", "convertedManaCost", "edhrecRank", "edhrecSaltiness",
+                "faceFlavorName","faceManaValue", "finishes", "frameEffects", "isAlternative", "isFullArt",
+                "isOnlineOnly", "isReprint", "isReserved", "keywords", "life", "manaCost", "manaValue",
+                "number", "originalPrintings", "originalReleaseDate", "otherFaceIds", "power", "toughness",
+                "printings", "purchaseUrls", "rarity", "side", "sourceProducts", "subsets", "supertypes", "text",
+                "type", "types", "artist", "artistIds"]    
 #open json file
-with open("data/AllPrintings.json", "r", encoding="utf-8") as f:
+def openJson():
+    with open("data/AllPrintings.json", "r", encoding="utf-8") as f:
+           data = json.load(f)
+    return data
 
-#unnest each set to get info I want
-    data = json.load(f)
-#list of keys(columns) I want and think are important for this project
-    keys = ["setCode", "uuid", "name", "borderColor", "colors", "convertedManaCost", "edhrecRank", "edhrecSaltiness",
-            "faceFlavorName","faceManaValue", "finishes", "frameEffects", "isAlternative", "isFullArt",
-            "isOnlineOnly", "isReprint", "isReserved", "keywords", "life", "manaCost", "manaValue",
-            "number", "originalPrintings", "originalReleaseDate", "otherFaceIds", "power", "toughness",
-            "printings", "purchaseUrls", "rarity", "side", "sourceProducts", "subsets", "supertypes", "text",
-            "type", "types", "artist", "artistIds"]    
+     
 
-
-#list that contains each sets dictionary values I want
-    final_list =[]
-
+def necessaryValue(json):
+    final_list = []
 #append that dic to the list
-    set_data = data["data"]
-    
+    set_data = json["data"]
     #key 10E, value.....
     # for each sets information, store the info of the key: cards and it's values(card data in a dic) in cards
     for sets_id, sets_info in set_data.items():
@@ -30,18 +30,33 @@ with open("data/AllPrintings.json", "r", encoding="utf-8") as f:
         #cards is a list of dictionary info of each card, for each card in this list store the key value pair into card_info, and we loop through only the keys we want
         for card in cards:
            card_info = {k: card.get(k, None) for k in keys}
+           card_url = card_info.get('purchaseUrls',{})
+           if isinstance(card_url, dict):
+               for site, url in card_url.items():
+                   card_info[site] = url
+               
+           card_info.pop("purchaseUrls", None)   
+              
            final_list.append(card_info)
+    # final_list.pop('purchaseUrls')
+    return final_list
+    
+def save_to_csv(ls):
+    df = pd.DataFrame(ls)
 
-df = pd.DataFrame(final_list)
+    df.to_csv("output/test_mtg.csv",  index=False, encoding="utf-8-sig")
 
-df.to_csv("output/test_mtg.csv",  index=False, encoding="utf-8-sig")
-
-print(f"total cards processed: {len(df)}")
+    print(f"total cards processed: {len(df)}")
 
 
+def main():
+    data = openJson()
+    mainValue = necessaryValue(data)
+    save_to_csv(mainValue)
 
+if __name__ == "__main__":
+    main()
 
 
     
-
 
