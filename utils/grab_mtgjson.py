03c61b735a046a52d740f2156b54bbc1d016403f
Josh <notlikethis1212@gmail.com>
2025-06-25 22:54:13 -0500

    update grab json file

--- utils/grab_mtgjson.py
+++ utils/grab_mtgjson.py
@@ -1,33 +1,40 @@
 import requests
 import os
 
-url = "https://mtgjson.com/api/v5/AllPrintings.json"
-output_path = "data/AllPrintings.json"
+
+def getUrl():
+    url = "https://mtgjson.com/api/v5/AllPrintings.json"
+    output_path = "data/AllPrintings.json"
 
 
-response = requests.get(url)
+    response = requests.get(url)
 
-if response.status_code == 200:
-    new_data = response.content
-    print("Download Success to memory")
-else:
-    print("Download Failed:", response.status_code)
+    if response.status_code == 200:
+        new_data = response.content
+        print("Download Success to memory")
+    else:
+        print("Download Failed:", response.status_code)
 
 
-if os.path.exists(output_path):
-    with open(output_path, "rb") as f:
-        current_data = f.read()
+    if os.path.exists(output_path):
+        with open(output_path, "rb") as f:
+            current_data = f.read()
 
-    if new_data == current_data:
-        print("No changes -- download skipped today")
-    
+        if new_data == current_data:
+            print("No changes -- download skipped today")
+        
+        else:
+            with open(output_path, "wb") as f:
+                f.write(new_data)
+            print("Chnages detected, updating new data")
     else:
         with open(output_path, "wb") as f:
             f.write(new_data)
-        print("Chnages detected, updating new data")
-else:
-    with open(output_path, "wb") as f:
-        f.write(new_data)
-    print("File does not exist, download and create")
+        print("File does not exist, download and create")
+
+def main():
+    getUrl()
 
 
+if __name__ == "__main__":
+    main()
